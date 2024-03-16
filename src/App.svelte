d36d90c8332db11058aaa335819113ebd0d47efc
Marianna Mazzette <20104120@mail.wit.ie>
2024-03-16 22:12:35 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<div class="container">
+  <div class="box has-text-centered">
+    <div class="title"> Simple Todo List</div>
+    <div class="subtitle">Fun things to do</div>
+  </div>
+</div>
