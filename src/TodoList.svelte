dd643341be1523a8a966acbc0f997394bbc69a5c
Marianna Mazzette <20104120@mail.wit.ie>
2024-03-22 21:32:16 +0000

    added components title and todolist

---
+++ src/TodoList.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let caption;
+  export let items = [];
+</script>
+
+<div class="section box">
+  <div class="title is-6">{caption}</div>
+  <table class="table is-fullwidth">
+    <thead>
+      <th>Task</th>
+      <th>Date</th>
+      <th></th>
+    </thead>
+    <tbody>
+      {#each items as todo}
+        <tr>
+          <td> {todo.text} </td>
+          <td> {todo.date}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
